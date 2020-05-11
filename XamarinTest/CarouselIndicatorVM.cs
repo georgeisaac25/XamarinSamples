b0ebaf94310832416041b4c800f7efa7055eb070
George <george.globant@vipper.com>
2020-05-11 21:01:00 +0530

    * CarouselIndicatorVM.cs: * CarouselViewExample.xaml: * XamarinTest.iOS.csproj: * CarouselViewWithCustomIndicator.xaml: * Contents.json: * Contents.json: * Contents.json: * Contents.json: * selected_circle.png: * selected_circle-1.png: * selected_circle-2.png: * unselected_circle.png: * unselected_circle-1.png: * unselected_circle-2.png:
    * MainActivity.cs: Added button
    
    * Info.plist: added

--- XamarinTest/CarouselIndicatorVM.cs
+++ XamarinTest/CarouselIndicatorVM.cs
@@ -2,19 +2,47 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace XamarinTest
 {
     public class CarouselIndicatorVM : INotifyPropertyChanged
     {
+        public ICommand NextClickCommand { get; private set; }
+        public ICommand PreviousClickCommand { get; private set; }
         public CarouselIndicatorVM()
         {
             MyDataSource = new List<CarouselData>() { new CarouselData() { Name = "Title11" },
                                                       new CarouselData() { Name = "Title2" },
                                                       new CarouselData() { Name = "Title3" },
-                                                      new CarouselData() { Name = "Title4" }};
+                                                      };
+            NextClickCommand = new Command(NextClick);
+
+            PreviousClickCommand = new Command(PreviousClick);
+
         }
+
+        void NextClick()
+        {
+            Console.WriteLine("next " + Position + MyDataSource.Count);
+            if (Position < MyDataSource.Count - 1)
+            {
+                Position += 1;
+            }
+
+        }
+
+        void PreviousClick()
+        {
+
+            if (Position != 0)
+            {
+                Position -= 1;
+            }
+        }
+
+
 
         public List<CarouselData> MyDataSource { get; set; } // Must have default value or be set before the BindingContext is set.
 
